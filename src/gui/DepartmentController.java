456904e83871f78dbe86caa986edc14700d3220e
Gabriel Kruger <krugergabriel27@outlook.com>
2023-07-12 19:34:36 -0300

    operation to create a new Department

--- src/gui/DepartmentController.java
+++ src/gui/DepartmentController.java
@@ -45,7 +45,8 @@ public class DepartmentController implements Initializable {
     @FXML
     public void onBtNewAction(ActionEvent event) {
         Stage parentStage = Utils.currentStage(event);
-        createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+        Department obj = new Department();
+        createDialogForm(obj,"/gui/DepartmentForm.fxml", parentStage);
     }
 
     public void setDepartmentService(DepartmentService service) {
@@ -65,10 +66,15 @@ public class DepartmentController implements Initializable {
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
     }
 
-    public void createDialogForm(String absoluteName, Stage parentStage) {
+    public void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane = loader.load();
+
+            DepartmentFormController controller = loader.getController();
+            controller.setDepartment(obj);
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateFormData();
 
             Stage dialogStage = new Stage();
             dialogStage.setScene(new Scene(pane));
