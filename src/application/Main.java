73a4d4be1bb88ad1a890bf099d8f077296a24f14
Gabriel Kruger <krugergabriel27@outlook.com>
2023-07-12 15:15:28 -0300

    Main view config and database acess

---
+++ src/application/Main.java
@@ -0,0 +1,44 @@
+package application;
+
+import gui.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+        AnchorPane root = loader.load();
+
+        ScrollPane scrollPane = (ScrollPane) root.lookup("#scrollPane");
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setTitle("Manager");
+
+        ((MainController) loader.getController()).init(stage);
+        stage.show();
+    }
+
+    public static Scene getScene(){
+        return scene;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/gui/MainController.java
@@ -0,0 +1,95 @@
+package gui;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.services.DepartmentService;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
+public class MainController implements Initializable {
+    //@FXML private ScrollPane scrollPane;
+    //@FXML private VBox vBox;
+    @FXML private Pane titlePane;
+    @FXML private ImageView btnMinimize, btnClose;
+
+    private double x, y;
+
+    public void init(Stage stage) {
+        titlePane.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+        titlePane.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX()-x);
+            stage.setY(mouseEvent.getScreenY()-y);
+        });
+
+        btnClose.setOnMouseClicked(mouseEvent -> stage.close());
+        btnMinimize.setOnMouseClicked(mouseEvent -> stage.setIconified(true));
+    }
+
+    @FXML private MenuItem menuItemSeller;
+    @FXML private MenuItem menuItemDepartment;
+    @FXML private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        loadView("/gui/Department.fxml", (DepartmentController controller) -> {
+            controller.setDepartmentService(new DepartmentService());
+            controller.updateTableView();
+        });
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        loadView("/gui/About.fxml", x -> {});
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene scene = Main.getScene();
+            AnchorPane anchorPane = (AnchorPane) scene.getRoot();
+            ScrollPane scrollPane = (ScrollPane) anchorPane.getChildren().get(0);
+            VBox contentVBox = (VBox) scrollPane.getContent();
+
+            Node mainMenu = contentVBox.getChildren().get(0);
+            contentVBox.getChildren().clear();
+            contentVBox.getChildren().add(mainMenu);
+            contentVBox.getChildren().addAll(newVBox.getChildren());
+
+            T controller = loader.getController();
+            initializingAction.accept(controller);
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+}
