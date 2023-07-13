a2e5e3b27fad872cd6f85f3d52b919657cf7e1ac
Gabriel Kruger <krugergabriel27@outlook.com>
2023-07-13 13:19:12 -0300

    Update Department

--- src/gui/DepartmentController.java
+++ src/gui/DepartmentController.java
@@ -1,15 +1,15 @@
 package gui;
 
-import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.StageStyle;
 import model.entities.Department;
@@ -18,9 +18,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
@@ -28,6 +25,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import static application.Main.getScene;
+
 
 public class DepartmentController implements Initializable, DataChangeListener {
 
@@ -38,6 +38,8 @@ public class DepartmentController implements Initializable, DataChangeListener {
     @FXML private TableColumn<Department, Integer> tableColumnId;
 
     @FXML private TableColumn<Department, String> tableColumnName;
+
+    @FXML private TableColumn<Department, Department> tableColumnEdit;
 
     @FXML private Button btNew;
 
@@ -63,7 +65,7 @@ public class DepartmentController implements Initializable, DataChangeListener {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 
-        Stage stage = (Stage) Main.getScene().getWindow();
+        Stage stage = (Stage) getScene().getWindow();
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
     }
 
@@ -98,6 +100,29 @@ public class DepartmentController implements Initializable, DataChangeListener {
         List<Department> list = service.findAll();
         observableList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(observableList);
+        initEditButtons();
+    }
+
+    private void initEditButtons() {
+        tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEdit.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("Editar");
+            private static final String BUTTON_STYLE_CLASS = "button-new";
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                setAlignment(Pos.CENTER);
+                button.getStyleClass().add(BUTTON_STYLE_CLASS);
+                button.setOnAction(
+                        event -> createDialogForm(
+                                obj, "/gui/DepartmentForm.fxml",Utils.currentStage(event)));
+            }
+        });
     }
 
     @Override
